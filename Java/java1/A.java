698c9050adde38f255d4b153bad8ac0d15aceabe
DonaldKingLiang <96967374+DonaldKingLiang@users.noreply.github.com>
2025-12-24 16:02:09 +0800

    update2025.12.24

--- Java/java1/A.java
+++ Java/java1/A.java
@@ -1,3 +1,20 @@
-class A{
-    
+class Rectangle{
+    double width;
+    double height;
+    double computCircum(){
+        return 2*(width+height);
+    }
+    double computArea(){
+        return width*height;
+    }
+    public static void main(String[] args){
+        int w=30;
+        int h=40;
+        Rectangle z=new Rectangle();
+        z.width=w;
+        z.height=h;
+        System.out.println(z.computCircum());
+        System.out.println(z.computArea());
+
+    }
 }
--- Java/java1/main.java
+++ Java/java1/main.java
@@ -1,5 +1,20 @@
-public class main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+class Rectangle{
+    double width;
+    double height;
+    double computCircum(){
+        return 2*(width+height);
+    }
+    double computArea(){
+        return width*height;
+    }
+    public static void main(String[] args){
+        int w=30;
+        int h=40;
+        Rectangle z=new Rectangle();
+        z.width=w;
+        z.height=h;
+        System.out.println(z.computCircum());
+        System.out.println(z.computArea());
+
     }
 }
